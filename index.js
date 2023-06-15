2f2521c1f12cd0f02bf540ffe6936c6f3763ace8
CNdumi <carolinendumi12@gmail.com>
2023-06-15 20:55:20 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(list, message){
+    let thankYouMessages = [];
+    for (let i=0; i<list.length; i++) {
+        thankYouMessages.push(`Thank you, ${list[i]}, for the wonderful ${message} gift!`);
+    }
+    return thankYouMessages
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+}
